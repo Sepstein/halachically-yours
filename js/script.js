51791472c535574b7ed7cce91b4f8939dedb0fa9
Sam <snepstein@gmail.com>
2013-06-09 21:17:46 -0500

    Finally got my jQuery working. All is well.

--- js/script.js
+++ js/script.js
@@ -1,1 +1,10 @@
- 
+$(document).ready(function() {
+    $('#navbar ul li').mouseenter(function() {
+	$(this).css('background-color','teal');
+	$(this).css('color','white');
+    });
+    $('#navbar ul li').mouseleave(function() {
+	$(this).css('background-color','#F0F0F0');
+	$(this).css('color','black');
+    });
+});
